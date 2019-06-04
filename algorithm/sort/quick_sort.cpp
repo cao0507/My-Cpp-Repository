7d0ba985514f827a2666d7e65d1ed77816c23623
cao0507 <1354100690@qq.com>
2019-06-04 11:02:09 +0800

    add multiplexing_IO

--- algorithm/sort/quick_sort.cpp
+++ algorithm/sort/quick_sort.cpp
@@ -1,6 +1,36 @@
 #include "sort.h"
 
-void Quick_sort::quick_sort(vector<int> &nums){
+/*
+ * 快速排序：不是稳定的，不稳定发生在主元与nums[j]交换的时刻。
+ */
+
+
+//void Quick_sort::quick_sort(vector<int> &nums){
+//	_quick_sort(nums, 0, nums.size()-1);
+//}
+//
+//void Quick_sort::_quick_sort(vector<int> &nums, int p, int r){
+//	if (p < r){
+//		int q = _partition(nums, p, r);
+//		_quick_sort(nums, p, q-1);
+//		_quick_sort(nums, q+1, r);
+//	}
+//}
+//
+//int Quick_sort::_partition(vector<int> &nums, int p, int r){
+//	int x = nums[r];
+//	int i = p-1;
+//	for (int j = p; j < r; j++){
+//		if (nums[j] <= x){
+//			i = i+1;
+//			swap(nums[i], nums[j]);
+//		}
+//	}
+//	swap(nums[i+1], nums[r]);
+//	return i+1;
+//}
+
+void Quick_sort::quick_sort( vector<int> &nums ){
 	_quick_sort(nums, 0, nums.size()-1);
 }
 
@@ -13,14 +43,22 @@ void Quick_sort::_quick_sort(vector<int> &nums, int p, int r){
 }
 
 int Quick_sort::_partition(vector<int> &nums, int p, int r){
-	int x = nums[r];
-	int i = p-1;
-	for (int j = p; j < r; j++){
-		if (nums[j] <= x){
-			i = i+1;
+	int x = nums[p];
+	int i = p+1, j = r;
+	while (1){
+		while (x >= nums[i])
+			i++;
+		while (x < nums[j])
+			j--;
+		if (i <= j){
 			swap(nums[i], nums[j]);
+			i++;
+			j--;
+		}
+		else{
+			swap(nums[p], nums[j]);
+			break;
 		}
 	}
-	swap(nums[i+1], nums[r]);
-	return i+1;
+	return j;
 }
